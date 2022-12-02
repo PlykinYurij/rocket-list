b436851999f211062544da6175e43b0390f04744
PlykinYura <plikin.yurij@yandex.ru>
2022-12-02 22:37:15 +0400

    changet preloader

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import preloader from "../../../assets/images/loader.gif";
+import classes from "./Preloader.module.css"
 
 const Preloader = () => {
-    return <img src={preloader} width="50px"/>; 
+    return <div className={classes.container}>
+        <div className={classes.preloaderImg}><img src={preloader} /></div>
+        <div className={classes.preloaderText}>подождите, идет загрузка...</div>
+    </div>
+
+
 };
 
 export default Preloader;
